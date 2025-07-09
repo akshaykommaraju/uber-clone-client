a3757c55614168689ae9ee4ff867efe6d98ffff1
akshay kommaraju <akshaychowdary012@gmail.com>
2025-07-09 17:33:25 +0530

    Added Footer component

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-5">
+      © 2025 QuickRide. All rights reserved.
+    </footer>
+  );
+}
